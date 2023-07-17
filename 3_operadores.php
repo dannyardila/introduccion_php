9b0769119f22e6e2499ca8d1d013ff8aeeeb471a
Tu Nombre <dannyardila059@gmail.com>
2023-07-17 18:39:10 -0500

    operadores php lo que llevo

---
+++ 3_operadores.php
@@ -0,0 +1,73 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores</title>
+</head>
+<body>
+    <h1>Operadores</h1>
+    
+    <?php
+     // Operadores aritmeticos:son símbolos especiales que se utilizan para realizar operaciones matemáticas en variables y valores numéricos. Estos operadores permiten realizar acciones como sumar, restar, multiplicar, dividir y obtener el residuo de una división.Ejemplo//
+
+    //Suma (+): Se utiliza para sumar dos valores.
+    //Ejemplo: $a + $b//
+
+   // suma (+): Se utiliza para sumar dos valores.
+
+    $a = 5;
+    $b = 100;
+    $resultado = $a + $b; // El valor de $resultado será 105
+
+   // Resta (-): Se utiliza para restar un valor de otro.
+  //Ejemplo: $a - $b//
+
+  $a = 5;
+  $b = 5;
+  $resultado = $a * $b; // El valor de $resultado será 25
+
+    //Multiplicación (*): Se utiliza para multiplicar dos valores.
+    Ejemplo: $a * $b
+
+//División (/): Se utiliza para dividir un valor entre otro.
+//Ejemplo: $a / $b
+
+//Módulo (%): Se utiliza para obtener el residuo de una división.
+//Ejemplo: $a % $b//
+
+//Exponenciación (**): Se utiliza para elevar un número a una potencia.
+//Ejemplo: $a ** $b//
+
+
+
+    // Operadores de comparacion:Son una forma concisa de combinar una operación y una asignación en una sola expresión. Estos operadores realizan una operación aritmética o de bits y, a continuación, asignan el resultado a la misma variable.Ejemplo
+    //Operador de combinación de suma (+=): Suma un valor a una variable y asigna el resultado a la misma variable.
+
+    //Operador de combinación de suma (+=): Suma un valor a una variable y asigna el resultado a la misma variable.
+   // Ejemplo: $a += $b; es equivalente a $a = $a + $b;//
+
+    //Operador de combinación de resta (-=): Resta un valor de una variable y asigna el resultado a la misma variable.
+    //Ejemplo: $a -= $b; es equivalente a $a = $a - $b;//
+
+    //Operador de combinación de multiplicación (*=): Multiplica una variable por un valor y asigna el resultado a la misma variable.
+    //Ejemplo: $a *= $b; es equivalente a $a = $a * $b;//
+
+    //Operador de combinación de división (/=): Divide una variable por un valor y asigna el resultado a la misma variable.
+    //jemplo: $a /= $b; es equivalente a $a = $a / $b;//
+
+    //Operador de combinación de módulo (%=): Calcula el módulo de una variable con un valor y asigna el resultado a la misma variable.
+    //Ejemplo: $a %= $b; es equivalente a $a = $a % $b;//
+
+    //Operador de combinación de concatenación (.=): Concatena una cadena de texto a una variable y asigna el resultado a la misma variable.
+    //Ejemplo: $a .= $b; es equivalente a $a = $a . $b;//
+
+    //operadores logicos:Se utilizan para combinar expresiones lógicas y evaluar condiciones booleanas. Estos operadores permiten realizar comparaciones y tomar decisiones basadas en el resultado de esas comparaciones. Los operadores lógicos disponibles en PHP son los siguientes://
+
+    ?>
+
+   
+
+    </scrit-->
+</body>
+</html>
